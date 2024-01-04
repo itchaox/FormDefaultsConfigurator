13090303ea0e5f4e114445cc06b62fddb2514aa2
itchaox <1462831573@qq.com>
2024-01-05 01:07:18 +0800

    init: 初始化项目

---
+++ src/components/Form.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Version    : v1.00
+ * @Author     : itchaox
+ * @Date       : 2023-12-23 09:34
+ * @LastAuthor : itchaox
+ * @LastTime   : 2023-12-24 13:37
+ * @desc       : 
+-->
+
+<script setup lang="ts">
+  import { bitable } from '@lark-base-open/js-sdk';
+</script>
+
+<template>
+  <div class="home">插件开发模板 Vue3</div>
+</template>
+
+<style scoped>
+  .home {
+    font-size: 14px;
+  }
+</style>
