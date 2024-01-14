aa8747629b1fa0e8529aeeecb36b455ca92ab375
itchaox <1462831573@qq.com>
2024-01-14 15:45:59 +0800

    feat: 英文显示兼容

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
  * @Author     : itchaox
  * @Date       : 2023-09-25 20:55
  * @LastAuthor : itchaox
- * @LastTime   : 2024-01-14 15:18
+ * @LastTime   : 2024-01-14 15:43
  * @desc       :
  */
 import { createApp } from 'vue';
@@ -15,6 +15,9 @@ import { i18n } from './locales/i18n.js';
 
 import ElementPlus from 'element-plus';
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
+import en from 'element-plus/dist/locale/en.mjs';
+
+import { bitable } from '@lark-base-open/js-sdk';
 
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
@@ -23,8 +26,13 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
 
-app.use(ElementPlus, {
-  locale: zhCn,
+bitable.bridge.getLanguage().then((lang) => {
+  i18n.global.locale = lang;
+  const _isZh = lang === 'zh' || lang === 'zh-HK' || lang === 'zh-TW';
+
+  app.use(ElementPlus, {
+    locale: _isZh ? zhCn : en,
+  });
 });
 
 app.use(i18n);
