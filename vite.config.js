029752411ec340810b30068d867c70afc8c0370a
wangchao <wangchao2423@gmail.com>
2024-03-28 15:06:11 +0800

    feat: 修改 base 静态资源路径

--- vite.config.js
+++ vite.config.js
@@ -2,34 +2,35 @@
  * @Version    : v1.00
  * @Author     : itchaox
  * @Date       : 2023-06-21 11:48
- * @LastAuthor : itchaox
- * @LastTime   : 2023-12-22 00:30
+ * @LastAuthor : wangchao
+ * @LastTime   : 2024-03-28 15:04
  * @desc       :
  */
 
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import AutoImport from 'unplugin-auto-import/vite';
-import Components from 'unplugin-vue-components/vite';
-import vueJsx from '@vitejs/plugin-vue-jsx';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   server: {
     host: true,
     hmr: true, //启动热更新，就是更改了代码自动刷新页面
     // open: true, //代表vite项目在启动时自动打开浏览器
     proxy: {
-      '/api': {
-        target: 'https://open.feishu.cn',
+      "/api": {
+        target: "https://open.feishu.cn",
         //你的需要请求的服务器地址
         changeOrigin: true, // 允许跨域
         secure: false, //忽略安全证书
-        rewrite: (path) => path.replace(/^\/api/, ''), // 重写路径把路径变成空字符,
+        rewrite: (path) => path.replace(/^\/api/, ""), // 重写路径把路径变成空字符,
       },
     },
   },
@@ -38,7 +39,7 @@ export default defineConfig({
     vueJsx(),
 
     AutoImport({
-      imports: ['vue', 'vue-router', 'pinia'],
+      imports: ["vue", "vue-router", "pinia"],
       resolvers: [ElementPlusResolver()],
     }),
     Components({
@@ -47,7 +48,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
